5d0454e0a43876cf72d1c0b56a9089163cd97669
K_ar_el <karel.verhulst@student.howest.be>
2021-06-20 20:43:18 +0200

    improve some things

---
+++ TrySomething/main.js
@@ -0,0 +1,42 @@
+const clc = document.querySelector(".cancel");
+		const arr = document.querySelector(".arr_container");
+		const left_container = document.querySelector(".left_container");
+
+		arr.addEventListener("click", ()=> {
+			arr.classList.add("active_arr");
+
+			if (left_container.classList.contains("off")) {
+				left_container.classList.remove("off");
+				left_container.classList.add("active");
+			}
+		});
+
+		clc.addEventListener("click", ()=> {
+			arr.classList.remove("active_arr");
+
+			if (left_container.classList.contains("active")) {
+				left_container.classList.remove("active");
+				left_container.classList.add("off");
+			}
+		});
+
+		videoSettingWithMouse("video1");
+		videoSettingWithMouse("video2");
+
+		function videoSettingWithMouse(id) {
+			document.getElementById(id).addEventListener("mouseover", function() {
+				//this.play();
+				this.childNodes[5].play();
+				console.log(this.childNodes);
+			});
+
+			document.getElementById(id).addEventListener("mouseleave", function() {
+				//this.pause();
+				this.childNodes[5].pause();
+			});
+
+			document.getElementById(id).addEventListener("click", function() {
+				//this.pause();
+				this.childNodes[5].play();
+			});
+		}
